783fb37cf78f616c30a897791192b529f8a1a77f
Abass <abass_gbolahan@yahoo.com>
2023-03-18 23:00:35 +0000

    base components created

---
+++ src/components/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onClick()" class="btn" :style="{ 'background-color': color }">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    text: String,
+    color: String,
+  },
+
+  methods: {
+    onClick() {},
+  },
+};
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <AppButton text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import AppButton from "./AppButton.vue";
+export default {
+  name: "AppHeader",
+  props: {
+    title: String,
+  },
+  components: { AppButton },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
